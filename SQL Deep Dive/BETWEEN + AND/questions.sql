c1cafdb752c4c6a3dc5691fe14fa2b72ee4287cf
Nasa-ee Madadam <122335609+Nasaee@users.noreply.github.com>
2024-01-26 11:49:14 +0700

    Update questions.sql

--- SQL Deep Dive/BETWEEN + AND/questions.sql
+++ SQL Deep Dive/BETWEEN + AND/questions.sql
@@ -3,9 +3,14 @@
 
 /*
 * Write your query here
+SELECT * FROM customers
 */
+SELECT * FROM customers
+WHERE age BETWEEN 30 and 50 and income < 50000;
 
 -- What is the average income between the ages of 20 and 50? (Including 20 and 50)
 /*
 * Write your query here
 */
+SELECT avg(income) FROM customers
+WHERE age BETWEEN 20 and 50;
