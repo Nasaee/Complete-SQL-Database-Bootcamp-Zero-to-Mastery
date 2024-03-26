b308b0b43c27a4951640ac08c0c58f24168a3b5e
Nasa-ee Madadam <122335609+Nasaee@users.noreply.github.com>
2024-03-26 14:35:49 +0700

    Update questions.sql

--- SQL Deep Dive/Null Values/questions.sql
+++ SQL Deep Dive/Null Values/questions.sql
@@ -4,7 +4,7 @@
 * Question: 
 * Assuming a students minimum age for the class is 15, what is the average age of a student?
 */
-
+SELECT avg(coalesce(age, 15)) from "Student";
 /*
 * DB: https://www.db-fiddle.com/f/PnGNcaPYfGoEDvfexzEUA/0
 * Question: 
