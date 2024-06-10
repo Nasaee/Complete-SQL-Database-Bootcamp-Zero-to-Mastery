039f4e4feb0062f3bc7e0602fedcb7268140e1cb
Nasa-ee Madadam <122335609+Nasaee@users.noreply.github.com>
2024-06-10 16:01:34 +0700

    Update questions.sql

--- SQL Deep Dive/Date Filtering/questions.sql
+++ SQL Deep Dive/Date Filtering/questions.sql
@@ -4,15 +4,23 @@
 * Question: Get me all the employees above 60, use the appropriate date functions
 */
 
-SELECT * FROM employees;
+SELECT AGE(birth_date), * FROM employees
+WHERE (
+   EXTRACT (YEAR FROM AGE(birth_date))
+) > 60 ;
 
+-- alternative
+SELECT count(birth_date) FROM employees
+WHERE birth_date < now() - interval '61 years' -- 61 years before the current date
+  
 /*
 * DB: Employees
 * Table: employees
 * Question: How many employees where hired in February?
 */
 
-SELECT * FROM employees;
+SELECT count(emp_no) FROM employees
+where EXTRACT (MONTH FROM hire_date) = 2;
 
 /*
 * DB: Employees
@@ -20,7 +28,8 @@ SELECT * FROM employees;
 * Question: How many employees were born in november?
 */
 
-SELECT * FROM employees;
+SELECT COUNT(emp_no) FROM employees
+WHERE EXTRACT (MONTH FROM birth_date) = 11;
 
 /*
 * DB: Employees
@@ -28,7 +37,7 @@ SELECT * FROM employees;
 * Question: Who is the oldest employee? (Use the analytical function MAX)
 */
 
-SELECT * FROM employees;
+SELECT MAX(AGE(birth_date)) FROM employees;
 
 /*
 * DB: Store
@@ -36,5 +45,7 @@ SELECT * FROM employees;
 * Question: How many orders were made in January 2004?
 */
 
-SELECT * FROM orders;
+SELECT COUNT(orderid)
+FROM orders
+WHERE DATE_TRUNC('month', orderdate) = date '2004-01-01';
 
