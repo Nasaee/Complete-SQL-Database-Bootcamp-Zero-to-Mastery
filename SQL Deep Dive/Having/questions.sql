70a519264ebb7dc32bb200bb4b00ed2bd22014da
Nasa-ee Madadam <122335609+Nasaee@users.noreply.github.com>
2024-09-25 13:44:16 +0700

    Update questions.sql

--- SQL Deep Dive/Having/questions.sql
+++ SQL Deep Dive/Having/questions.sql
@@ -14,7 +14,12 @@ HAVING count(t.title) > 2
 *  Show me all the employees that have had more than 15 salary changes that work in the department development
 *  Database: Employees
 */
-
+SELECT e.emp_no, count(s.salary) from employees as e
+JOIN salaries as s using(emp_no)
+join dept_emp as de using(emp_no)
+where de.dept_no = 'd005'
+GROUP by e.emp_no
+HAVING count(s.salary) > 15
 
 
 /*
